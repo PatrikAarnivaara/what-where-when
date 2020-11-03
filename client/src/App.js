ee7f5e5f4ff27623c6375d0f1dbd60a434442d4a
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-11-03 21:03:51 +0100

    Added switch for dark/light theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import {
   BrowserRouter as Router,
   Route,
   NavLink,
-  Switch,
+  /* Switch */
 } from "react-router-dom";
+import { Switch } from "@material-ui/core/";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 /* import Uploads from "../src/components/Uploads" */
 import Header from "../src/components/Header/Header";
 import Gallery from "../src/components/Gallery";
@@ -13,15 +17,25 @@ import ImageList from "../src/components/ImageList";
 /* import UserForm from "../src/components/UserForm" */
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <div className="App">
-      <Header />
-      <UploadForm />
-      <ImageList />
-      {/* <Uploads/> */}
-      {/*      <Gallery/>
-       */}{" "}
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Header />
+        <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+        <UploadForm />
+        <ImageList />
+      </div>
+    </ThemeProvider>
   );
 }
 
