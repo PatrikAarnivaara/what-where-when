110e2d37f719815fa20c71dd4f1de4a20b04e6d2
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-11-03 21:31:05 +0100

    Added moon sun symbol that switches dependent on theme

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,8 +3,10 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
 import MenuIcon from "@material-ui/icons/Menu";
-import { Switch } from "@material-ui/core/";
+import { Button, Switch } from "@material-ui/core/";
 import useStyles from "./useStyle";
 
 const Header = ({ changeTheme, darkMode }) => {
@@ -29,7 +31,10 @@ const Header = ({ changeTheme, darkMode }) => {
           <Typography variant="h6" className={classes.title}>
             WwW
           </Typography>
-          <Switch checked={darkMode} onChange={switchTheme} />
+          {/* <Switch checked={darkMode} onChange={switchTheme} /> */}
+          <Button onClick={switchTheme}>
+            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
