b9e61d23d9e2fda5f26d9ff512f50e7e29844417
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-12-14 13:08:46 +0100

    Fixed sizing of image detail

--- client/src/components/predictions/PredictionListItemDetail.js
+++ client/src/components/predictions/PredictionListItemDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Typography } from '@material-ui/core/';
+import { Image } from 'cloudinary-react';
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import useStyles from './useStyles';
 
@@ -9,7 +10,15 @@ const PredictionListItemDetail = ({ predictionDetailData: { _id, url, title, des
 
 	return (
 		<div className={classes.itemDetailWrapper}>
-			<img src={url} alt={title} />
+			<Image
+				cloudName={process.env.REACT_APP_CLOUDINARY_NAME}
+				publicId={url}
+				width="180"
+				crop="scale"
+				quality="auto"
+			/>
+			
+			{/* <img src={url} alt={title} /> */}
 			<Typography>Your prediction: {title}</Typography>
 			<Typography>Classification: {description}</Typography>
 			<Typography>Probability: {probability * 100}%</Typography>
